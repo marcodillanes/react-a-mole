b1979dd7419be075886212c4ef553f9eb37ebd8d
Marco <114459798+marcodillanes@users.noreply.github.com>
2023-03-15 00:16:01 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import moleContainer from './MoleContainer';
 
 function App() {
+  let [score, setScore] = useState(0)
+
+  const createMoleHill = () => {
+    let hills = []
+    for(let i = 0; i < 9; i++) {
+      hills.push(<moleContainer key={i} setScore={setScore} score={score} />)
+    }
+    return (
+      <div>
+        { hills }
+      </div>
+    )
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React-A-Mole!</h1>
+      {score}
+      {createMoleHill()}
     </div>
   );
 }
 
 export default App;
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
